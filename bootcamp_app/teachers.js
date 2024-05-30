54aa0357250de196bc192616d80e9e8f5a602d4e
arpipatel-ap <arpi96@gmail.com>
2024-05-30 11:54:19 -0400

    add  parameterized queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,23 +7,22 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-pool
-  .query(
-    `
-    SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-    FROM teachers
-    JOIN assistance_requests ON teacher_id = teachers.id
-    JOIN students ON student_id = students.id
-    JOIN cohorts ON cohort_id = cohorts.id
-    WHERE cohorts.name = '${process.argv[2] || JUL02}'
-    ORDER BY teacher;
-    
-`
-  )
-  .then((res) => {
-   // console.log(res.rows);
-    for(let teacher of res.rows){
+let args = process.argv.slice(2);
+let values = [`${args[0]}`];
+pool.query(`
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name = $1
+ORDER BY teacher;
+`, values)
+
+  .then(res => {
+    console.log(res.rows);
+    for (let teacher of res.rows) {
       console.log(`${teacher.cohort}: ${teacher.teacher}`);
     }
   })
-  .catch((err) => console.error("query error", err.stack));
+  .catch(err => console.error('query error', err.stack));
